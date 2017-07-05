c07007129eb2e21d1f29f1624fa068473ba4f9ac
Che Hang <thss15_cheh@163.com>
2017-07-06 04:10:42 +0800

    add category add, get and remove

--- handlers/post_categories.py
+++ handlers/post_categories.py
@@ -1,6 +1,34 @@
-from models import PostCategories
+from models import PostCategories, User
+from .exception import InvalidRequest
+from aiohttp_session import get_session
 
+
+async def category_add(data, request):
+    post_category: PostCategories = request.app.models.post_categories
+    user: User = request.app.models.user
+    session = await get_session(request)
+    if 'uid' not in session or not (await user.is_administrator(session['uid'])):
+        raise InvalidRequest('Permission denied')
+    return await post_category.add(data.get('name'), data.get('parent'))
+
+async def category_get(data, request):
+    post_category: PostCategories = request.app.models.post_categories
+    return await post_category.get(data['name'])
+
+async def category_remove(data, request):
+    post_category: PostCategories = request.app.models.post_categories
+    user: User = request.app.models.user
+    session = await get_session(request)
+    if 'uid' not in session or not (await user.is_administrator(session['uid'])):
+        raise InvalidRequest('Permission denied')
+    await post_category.remove(data)
+
+async def category_update(data, request):
+    pass
 
 handlers = {
-
+    'category-add': (category_add, ('ajax-post',)),
+    'category-get': (category_get, ('ajax-get',)),
+    'category-remove': (category_remove, ('ajax-delete',)),
+    'category-update': (category_update, ('ajax-post',)),
 }
--- models/post_categories.py
+++ models/post_categories.py
@@ -1,4 +1,5 @@
 from motor import motor_asyncio
+from handlers.exception import InvalidRequest
 
 
 class PostCategories:
@@ -12,3 +13,44 @@ class PostCategories:
             upsert=True
         )
         await self.db.create_index('name')
+
+    async def add(self, name, parent):
+        if await self.db.find_one({'name': name}) is not None:
+            raise InvalidRequest('Category already exists')
+        if parent is None:
+            parent = '$root'
+        if await self.db.find_one_and_update(
+            {'name': parent},
+            {'$addToSet': {'children': name}}
+        ) is None:
+            raise InvalidRequest('Parent does not exists')
+        await self.db.insert_one({
+            'name': name,
+            'parent': parent
+        })
+
+    async def get(self, name):
+        return await self.db.find_one({'name': name}, projection={
+            '_id': False
+        })
+
+    async def remove_children(self, name):
+        children = (await self.db.find_one({'name': name})).get('children')
+        if children is not None:
+            for x in children:
+                await self.remove_children(x)
+        await self.db.find_one_and_delete({'name': name})
+
+    async def remove(self, name):
+        result = await self.db.find_one({'name': name})
+        if result is None:
+            raise InvalidRequest('Category does not exist')
+        children = result.get('children')
+        if children is not None:
+            for x in children:
+                await self.remove_children(x)
+        await self.db.find_one_and_delete({'name': name})
+        await self.db.find_one_and_update(
+            {'name': result['parent']},
+            {'$pullAll': {'children': [name]}}
+        )
