408703de24a258ad4de91f4cce4361e2047e8b2c
Che Hang <thss15_cheh@163.com>
2017-07-06 00:06:54 +0800

    add settings set and get

--- handlers/settings.py
+++ handlers/settings.py
@@ -2,9 +2,14 @@ from models import Settings
 
 # settings_set
 async def settings_get(data, request):
-    pass
+    settings: Settings = request.app.models.settings
+    return await settings.get()
 
+async def settings_set(data, request):
+    settings: Settings = request.app.models.settings
+    return await settings.set(data)
 
 handlers = {
     'settings-get': (settings_get, ('ajax-get',)),
+    'settings-set': (settings_set, ('ajax-post',)),
 }
--- models/settings.py
+++ models/settings.py
@@ -3,7 +3,21 @@ from motor import motor_asyncio
 
 class Settings:
     def __init__(self, models):
-        self.db: motor_asyncio.AsyncIOMotorCollection = models.db['user']
+        self.db: motor_asyncio.AsyncIOMotorCollection = models.db['settings']
 
     async def startup(self):
         await self.db.create_index('key')
+
+    async def get(self):
+        result = {}
+        async for record in self.db.find():
+            result[record['key']] = record['value']
+        return result
+
+    async def set(self, data):
+        for x in data.keys():
+            self.db.find_one_and_update(
+                {'key': x},
+                {'$set': {'value': data[x]}},
+                upsert=True
+            )
